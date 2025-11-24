024fe53e477273c197ae97270b3502074923a38f
dalcannon <36898468+dalcannon@users.noreply.github.com>
2025-11-24 17:14:53 -0500

    Add files via upload

---
+++ R/src/movmeanandstd.cpp
@@ -0,0 +1,47 @@
+#include <Rcpp.h>
+#include <cmath>
+using namespace Rcpp;
+// Function to calculate the rolling standard deviation
+// [[Rcpp::export]]
+List movmeanandstd(Rcpp::NumericMatrix X, int w) {
+  int m=X.nrow();
+  int n=X.ncol();
+  if (w <= 0 || w % 2 == 0) {
+    stop("w must be an odd positive integer.");
+  }
+  int v = (w - 1) / 2; // half-window size v = 2*w+1
+  //Calculate the rolling mean (rollmean)
+  Rcpp::NumericMatrix rollmean(m, n);
+  // Loop over each row in the matrix
+  for (int i = 0; i < m; ++i) {
+    // Loop over each column in the current row
+    for (int j = 0; j < n; ++j) {
+      double sum = 0.0;
+      int count = 0;
+      for (int k = std::max(0, j - v); k <= std::min(n - 1, j + v); ++k) {
+        sum += X(i, k);
+        ++count;
+      }
+      rollmean(i,j) = sum/count;
+    }
+  }
+  //now calculate the rolling standard deviation
+  Rcpp::NumericMatrix movsd(m, n);
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < n; ++j) {
+      double sumSquaredDiff = 0.0;
+      int count = 0;
+      for (int k = j - v; k <= j + v; ++k) {
+        if (k >= 0 && k < n) {
+          double diff = X(i,k) - rollmean(i,j);
+          sumSquaredDiff += diff * diff;
+          count++;
+        }
+      }
+      double variance = count > 0 ? sumSquaredDiff / (count - 1) : 0.0;
+      movsd(i,j) = count > 0 ? std::sqrt(variance) : NA_REAL;
+    }
+  }
+  return List::create(Named("mean") = rollmean,
+                      Named("sd") = movsd);
+}
---
+++ R/src/movsum.cpp
@@ -0,0 +1,25 @@
+#include <Rcpp.h>
+#include <cmath>
+using namespace Rcpp;
+// [[Rcpp::export]]
+NumericMatrix movsum(NumericMatrix X, int w) {
+  int m=X.nrow(); // Number of rows in X
+  int n=X.ncol(); // Number of columns in X
+  Rcpp::NumericMatrix rollsum(m, n);
+  if (w <= 0 || w % 2 == 0) {
+    stop("w must be an odd positive integer.");
+  }
+  int v = (w - 1) / 2; // half-window size v = 2*w+1
+  // Loop over each row in the matrix
+  for (int i = 0; i < m; ++i) {
+    // Loop over each column in the current row
+    for (int j = 0; j < n; ++j) {
+      double sum = 0.0;
+      for (int k = std::max(0, j - v); k <= std::min(n - 1, j + v); ++k) {
+        sum += X(i, k);
+      }
+      rollsum(i,j) = sum;
+    }
+  }
+  return wrap(rollsum);
+}
